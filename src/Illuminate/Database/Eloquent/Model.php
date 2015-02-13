4541fcc763a7c9e2b6cebf444630600bd1593ea4
Alex Slaughter <alexs@notebowl.com>
2015-02-13 06:45:56 -0700

    update

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1161,38 +1161,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	}
 
 	/**
-	 * Restore a soft-deleted model instance.
-	 *
-	 * @return bool|null
-	 */
-	public function restore()
-	{
-		if ($this->softDelete)
-		{
-			// If the restoring event does not return false, we will proceed with this
-			// restore operation. Otherwise, we bail out so the developer will stop
-			// the restore totally. We will clear the deleted timestamp and save.
-			if ($this->fireModelEvent('restoring') === false)
-			{
-				return false;
-			}
-
-			$this->{$this->getDeletedAtColumn()} = null;
-
-			// Once we have saved the model, we will fire the "restored" event so this
-			// developer will do anything they need to after a restore operation is
-			// totally finished. Then we will return the result of the save call.
-			$this->exists = true;
-
-			$result = $this->save();
-
-			$this->fireModelEvent('restored', false);
-
-			return $result;
-		}
-	}
-
-	/**
 	 * Register a saving model event with the dispatcher.
 	 *
 	 * @param  \Closure|string  $callback
--- src/Illuminate/Database/Eloquent/SoftDeletingTrait.php
+++ src/Illuminate/Database/Eloquent/SoftDeletingTrait.php
@@ -54,26 +54,29 @@ trait SoftDeletingTrait {
 	 */
 	public function restore()
 	{
-		// If the restoring event does not return false, we will proceed with this
-		// restore operation. Otherwise, we bail out so the developer will stop
-		// the restore totally. We will clear the deleted timestamp and save.
-		if ($this->fireModelEvent('restoring') === false)
+		if ($this->softDelete)
 		{
-			return false;
+			// If the restoring event does not return false, we will proceed with this
+			// restore operation. Otherwise, we bail out so the developer will stop
+			// the restore totally. We will clear the deleted timestamp and save.
+			if ($this->fireModelEvent('restoring') === false)
+			{
+				return false;
+			}
+
+			$this->{$this->getDeletedAtColumn()} = null;
+
+			// Once we have saved the model, we will fire the "restored" event so this
+			// developer will do anything they need to after a restore operation is
+			// totally finished. Then we will return the result of the save call.
+			$this->exists = true;
+
+			$result = $this->save();
+
+			$this->fireModelEvent('restored', false);
+
+			return $result;
 		}
-
-		$this->{$this->getDeletedAtColumn()} = null;
-
-		// Once we have saved the model, we will fire the "restored" event so this
-		// developer will do anything they need to after a restore operation is
-		// totally finished. Then we will return the result of the save call.
-		$this->exists = true;
-
-		$result = $this->save();
-
-		$this->fireModelEvent('restored', false);
-
-		return $result;
 	}
 
 	/**

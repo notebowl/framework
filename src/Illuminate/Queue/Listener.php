94483a96e2893d67a84fe663ebf1d500d788ec89
Alex Slaughter <alex@alexslaughter.com>
2016-09-12 18:14:52 -0400

    add jit false for hhvm

--- src/Illuminate/Queue/Listener.php
+++ src/Illuminate/Queue/Listener.php
@@ -73,7 +73,7 @@ class Listener
         $binary = ProcessUtils::escapeArgument((new PhpExecutableFinder)->find(false));
 
         if (defined('HHVM_VERSION')) {
-            $binary .= ' --php';
+            $binary .= ' -v Eval.Jit=false --php';
         }
 
         if (defined('ARTISAN_BINARY')) {

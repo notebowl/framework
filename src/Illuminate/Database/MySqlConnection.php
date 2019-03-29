337f43ce2a9a2e8153e34343def1e387d494a9a7
Alex Slaughter <alex@alexslaughter.com>
2019-03-28 18:26:57 -0700

    Update MySqlConnection.php

--- src/Illuminate/Database/MySqlConnection.php
+++ src/Illuminate/Database/MySqlConnection.php
@@ -64,21 +64,4 @@ class MySqlConnection extends Connection
     {
         return new DoctrineDriver;
     }
-
-    /**
-     * Bind values to their parameters in the given statement.
-     *
-     * @param  \PDOStatement $statement
-     * @param  array  $bindings
-     * @return void
-     */
-    public function bindValues($statement, $bindings)
-    {
-        foreach ($bindings as $key => $value) {
-            $statement->bindValue(
-                is_string($key) ? $key : $key + 1, $value,
-                is_int($value) || is_float($value) ? PDO::PARAM_INT : PDO::PARAM_STR
-            );
-        }
-    }
 }
